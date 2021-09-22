41f265ebdbccc59b423c5ccfcd48311cb5d81381
Joseph Kempster <jkempster34@gmail.com>
2021-09-22 14:21:12 +0100

    Move entire controller over from app
    Because the wizard is likely only used by the GiT services, move over some extra controller code to dry up the app.

--- lib/dfe_wizard/controller.rb
+++ lib/dfe_wizard/controller.rb
@@ -5,6 +5,8 @@ module DFEWizard
     included do
       class_attribute :wizard_class
       before_action :load_wizard, :load_current_step, only: %i[show update]
+      before_action :process_magic_link_token, only: %i[show]
+      before_action :display_magic_link_token_error, only: %i[show]
     end
 
     def index
@@ -31,7 +33,43 @@ module DFEWizard
       # current_step is loaded via before_action
     end
 
+    def resend_verification
+      request = GetIntoTeachingApiClient::ExistingCandidateRequest.new(camelized_identity_data)
+      GetIntoTeachingApiClient::CandidatesApi.new.create_candidate_access_token(request)
+      redirect_to authenticate_path(verification_resent: true)
+    rescue GetIntoTeachingApiClient::ApiError => e
+      redirect_to(first_step_path) && return if e.code == 400
+
+      raise
+    end
+
   private
+
+    def process_magic_link_token
+      token = request.query_parameters[:magic_link_token]
+      return if token.blank?
+
+      @wizard.process_magic_link_token(token)
+      redirect_to(step_path(@wizard.next_key(Wizard::Steps::Authenticate.key)))
+    rescue GetIntoTeachingApiClient::ApiError => e
+      handle_magic_link_token_error(e) && return if e.code == 401
+
+      raise
+    end
+
+    def handle_magic_link_token_error(error)
+      response = JSON.parse(error.response_body)
+      redirect_to(first_step_path(magic_link_token_error: response["status"]))
+    end
+
+    def display_magic_link_token_error
+      magic_link_token_error = params[:magic_link_token_error]
+      return if magic_link_token_error.blank?
+
+      key = "activemodel.errors.magic_link_token"
+      message = t("#{key}.#{magic_link_token_error.underscore}", default: t("#{key}.invalid"))
+      @current_step.flash_error(message)
+    end
 
     def wizard_store
       Store.new new_store, preexisting_store
@@ -39,6 +77,8 @@ module DFEWizard
 
     def load_wizard
       @wizard = wizard_class.new(wizard_store, params[:id])
+    rescue DFEWizard::UnknownStep
+      raise_not_found
     end
 
     def load_current_step
@@ -69,6 +109,15 @@ module DFEWizard
 
     def step_param_key
       @current_step.class.model_name.param_key
+    end
+
+    def authenticate_path(params = {})
+      step_path :authenticate, params
+    end
+
+    def camelized_identity_data
+      Wizard::Steps::Authenticate.new(nil, wizard_store)
+                                 .candidate_identity_data
     end
   end
 end
