b48d392be2df2c0dba5a18e18cb7ec1186435ae5
Joseph Kempster <jkempster34@gmail.com>
2021-09-30 13:59:31 +0100

    Remove unused dummy code

--- spec/dummy/app/controllers/event_steps_controller.rb
+++ spec/dummy/app/controllers/event_steps_controller.rb
@@ -2,54 +2,12 @@ class EventStepsController < ApplicationController
   include DFEWizard::Controller
   self.wizard_class = Events::Wizard
 
-#   before_action :redirect_closed_events, only: %i[show update]
-#   before_action :set_step_page_title, only: [:show]
-#   before_action :set_completed_page_title, only: [:completed]
-
-#   private
-
-#   def redirect_closed_events
-#     event_is_closed = @event.status_id == GetIntoTeachingApiClient::Constants::EVENT_STATUS["Closed"]
-#     candidate_is_walk_in = wizard_store[:is_walk_in]
-
-#     if event_is_closed && !candidate_is_walk_in
-#       redirect_to event_path(id: @event.readable_id)
-#     end
-#   end
-
   def step_path(step = params[:id], urlparams = {})
     event_step_path params[:event_id], step, urlparams
   end
   helper_method :step_path
 
-#   def completed_step_path
-#     if wizard_store["subscribe_to_mailing_list"]
-#       step_path :completed, subscribed: 1
-#     else
-#       step_path :completed
-#     end
-#   end
-
   def wizard_store
     ::DFEWizard::Store.new({}, {})
   end
-
-#   def load_event
-#     @event = { event: true }
-#   end
-
-#   def set_step_page_title
-#     @page_title = "Sign up for #{@event.name}"
-#     unless @current_step.nil?
-#       @page_title += ", #{@current_step.title.downcase} step"
-#     end
-#   end
-
-#   def set_completed_page_title
-#     @page_title = "Sign up complete"
-#   end
-
-#   def set_is_walk_in
-#     wizard_store[:is_walk_in] = true if params.key?(:walk_in)
-#   end
 end
