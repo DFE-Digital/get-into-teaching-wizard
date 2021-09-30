5c744468efe3a25e6a6d8c30a6b7d33d81deb247
Joseph Kempster <jkempster34@gmail.com>
2021-09-30 15:48:09 +0100

    Add new `Store#prune!` method from app

--- spec/dfe_wizard/store_spec.rb
+++ spec/dfe_wizard/store_spec.rb
@@ -124,12 +124,24 @@ describe DFEWizard::Store do
   end
 
   describe "#purge!" do
-    subject { instance.keys }
-
     before { instance.purge! }
+
+    subject { instance.keys }
 
     it "will remove all keys" do
       is_expected.to have_attributes empty?: true
+    end
+  end
+
+  describe "#prune!" do
+    subject { instance.keys }
+    let(:leave) { "age" }
+
+    before { instance.prune!(leave: leave) }
+
+    it "will remove all keys that aren't marked as 'leave'" do
+      expect(instance.keys).to include(leave)
+      expect(instance.keys).not_to include(new_data.keys.excluding(leave))
     end
   end
 end
