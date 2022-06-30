f0e70b8c17f70fafbc707ce77fab6f583936ab04
Ross Oliver <ross.oliver36@gmail.com>
2022-06-30 15:02:54 +0100

    Bump patch version for release

--- spec/dfe_wizard_spec.rb
+++ spec/dfe_wizard_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe DFEWizard do
   describe "VERSION" do
     subject { described_class::VERSION }
 
-    it { is_expected.to eql "1.0.0" }
+    it { is_expected.to eql "1.0.1" }
   end
 end
